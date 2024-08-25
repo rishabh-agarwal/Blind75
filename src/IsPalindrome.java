da961e499e4289561f3a8e696f99c1cda5b588f8
Rishabh Agarwal <rishabhagarwal@Rishabhs-MacBook-Air.local>
2024-08-24 22:48:44 -0500

    auto commit on 2024-08-24 22:48:44

---
+++ src/IsPalindrome.java
@@ -0,0 +1,25 @@
+/**
+ * Leetcode: 125
+ */
+public class IsPalindrome {
+    public static boolean isPalindrome(String s) {
+        char[] s_arr = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(char c: s_arr){
+            if(Character.isLetterOrDigit(c)){
+                sb.append(c);
+            }
+        }
+
+        String originalString = sb.toString();
+        String reverseString = sb.reverse().toString();
+
+        return originalString.equalsIgnoreCase(reverseString) ? true: false;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Was it a car or a cat I saw?"));
+        System.out.println(isPalindrome("tab a cat"));
+    }
+}
