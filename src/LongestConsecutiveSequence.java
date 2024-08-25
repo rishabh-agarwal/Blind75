b479c26327a0ab2ab475d246579bf22fe67f2b68
Rishabh Agarwal <rishabhagarwal@Rishabhs-MacBook-Air.local>
2024-08-24 22:29:07 -0500

    auto commit on 2024-08-24 22:29:07

---
+++ src/LongestConsecutiveSequence.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+/**
+ * Leetcode: 128
+ */
+public class LongestConsecutiveSequence {
+    public static int longestConsecutive(int[] nums) {
+        if(nums.length==0) return 0;
+
+        int maxSeq = 0;
+        int currSeq = 0;
+        Arrays.sort(nums);
+
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i+1]==nums[i]){
+                continue;
+            }
+            else if(nums[i+1]-nums[i]==1){
+                currSeq ++;
+            }else{
+                currSeq = 0;
+            }
+            maxSeq = Math.max(currSeq, maxSeq);
+        }
+
+        return maxSeq+1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestConsecutive(new int[]{2, 20, 4, 10, 3, 4, 5}));
+    }
+}
