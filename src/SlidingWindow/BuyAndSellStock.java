ce4aed5250a7dcc128f576fb5312eb36a2565461
Rishabh Agarwal <rishabhagarwal@Rishabhs-MacBook-Air.local>
2024-08-25 20:07:14 -0500

    auto commit on 2024-08-25 20:07:14

--- src/SlidingWindow/BuyAndSellStock.java
+++ src/SlidingWindow/BuyAndSellStock.java
@@ -4,7 +4,22 @@ package SlidingWindow;
  * Leetcode: 121
  **/
 public class BuyAndSellStock {
-    public int maxProfit(int[] prices) {
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int currProfit;
+        int len = prices.length;
+        for(int buy=0; buy<len-1; buy++){
+            int sell = buy+1;
+            while (sell < len) {
+                currProfit = prices[sell] - prices[buy];
+                maxProfit = Math.max(currProfit, maxProfit);
+                sell++;
+            }
+        }
+        return maxProfit;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{10,1,5,6,7,1}));
     }
 }
