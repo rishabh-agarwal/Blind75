bb133379779758068e9fdf78b1f4bc39ca1454ae
Rishabh Agarwal <rishabhagarwal@Rishabhs-MacBook-Air.local>
2024-09-02 23:57:00 -0500

    auto commit on 2024-09-02 23:57:00

---
+++ src/LinkedinList/ReverseALinkedList.java
@@ -0,0 +1,30 @@
+package LinkedinList;
+
+/**
+ * Leetcode: 206
+ **/
+
+public class ReverseALinkedList {
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode temp = null;
+
+        while(head!=null){
+            temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+
+        return head;
+    }
+}
