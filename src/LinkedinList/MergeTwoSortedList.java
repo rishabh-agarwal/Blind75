a118cfd795f5a214efd2e813291fda2e90cafad9
Rishabh Agarwal <rishabhagarwal@Rishabhs-MacBook-Air.local>
2024-09-03 00:20:38 -0500

    auto commit on 2024-09-03 00:20:38

--- src/LinkedinList/MergeTwoSortedList.java
+++ src/LinkedinList/MergeTwoSortedList.java
@@ -1,10 +1,31 @@
 package LinkedinList;
 
+import java.awt.*;
+
 /**
  * Leetcode: 21
  **/
 public class MergeTwoSortedList {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode newNode = new ListNode(0);
+        ListNode currPtr = newNode;
 
+        while(list1!=null && list2!=null){
+            if(list1.val < list2.val){
+                currPtr.next = list1;
+                list1 = list1.next;
+            }else{
+                currPtr.next = list2;
+                list2 = list2.next;
+            }
+            currPtr = currPtr.next;
+        }
+
+        if(list1 != null)
+            currPtr.next = list1;
+        if(list2 != null)
+            currPtr.next = list2;
+
+        return newNode.next;
     }
 }
