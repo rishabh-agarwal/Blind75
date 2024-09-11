3ac4dcdde35bda6cfaf2bcd03d4cd56690d846bb
Rishabh Agarwal <rishabhagarwal@Rishabhs-MacBook-Air.local>
2024-09-11 04:14:04 -0500

    auto commit on 2024-09-11 04:14:04

---
+++ src/LinkedinList/LinkedListCycleDetection.java
@@ -0,0 +1,18 @@
+package LinkedinList;
+
+/**
+ * Leetcode: 141
+ **/
+public class LinkedListCycleDetection {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast==slow) return true;
+        }
+        return false;
+    }
+}
